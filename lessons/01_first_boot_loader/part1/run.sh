39ab711a8c4bb97a4d3d10a4c02e0b7c98f0aa37
ALMaclaine <almaclaine@gmail.com>
2019-12-21 21:07:33 -0800

    add run script

---
+++ lessons/01_first_boot_loader/part1/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ ! `command -v qemu-system-x86_64` ]
+then
+  printf "Install qemu\n"
+  exit 1
+fi
+
+NAME=boot1
+
+if [ -f dist/$NAME.bin ]
+then
+  rm dist/$NAME.bin
+fi
+
+./compile.sh
+qemu-system-x86_64 -fda dist/$NAME.bin
